c186120315f69e57295bf39ccf142524b1be2162
guillermosanchez4878-netizen <guillermo.sanchez4878@alumnos.udg.mx>
2025-10-24 14:53:28 -0600

    Update models.py

--- models.py
+++ models.py
@@ -9,13 +9,13 @@ class Juego(db.Model):
     descripcion = db.Column(db.String(255), nullable=False)
     precio = db.Column(db.Numeric(9,2), nullable=False)
 
-# Clase User para la autenticación
+
 class User(UserMixin, db.Model): 
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True, nullable=False)
     
-    # CORRECCIÓN: Aumentado de 128 a 255 para evitar DataError
+    
     password_hash = db.Column(db.String(255)) 
 
     @property
@@ -24,12 +24,13 @@ class User(UserMixin, db.Model):
 
     @password.setter
     def password(self, password):
-        # Encripta la contraseña antes de guardarla
+       
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        # Verifica la contraseña ingresada contra el hash guardado
+    
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
+
         return f'<User {self.username}>'
